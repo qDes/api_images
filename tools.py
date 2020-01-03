c208fd18666ef96faddb238f9d7a06238926949e
Andrei K <1____1@bk.ru>
2020-01-03 15:15:36 +0300

    replace debug prints with logger

--- tools.py
+++ tools.py
@@ -1,22 +1,26 @@
 import requests
 import os
+import logging
 
 from PIL import Image
 
 
 def get_content(url, payload={}):
+    # FORMAT = '%(levelname): %(message)'
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('request')
     try:
         cafile = 'cacert.pem'  # http://curl.haxx.se/ca/cacert.pem
         response = requests.get(url, timeout=30, verify=cafile,
                                 params=payload)
         response.raise_for_status()
     except requests.Timeout:
-        print(f"timeout error: {url}")
+        logger.debug(f"timeout error: {url}")
     except requests.HTTPError as err:
         code = err.response.status_code
-        print(f"error url: {url}, code: {code}")
+        logger.debug(f"error url: {url}, code: {code}")
     except requests.RequestException as err:
-        print(f"{err} error; url: {url}")
+        logger.debug(f"{err} error; url: {url}")
     else:
         return response.content
 
